754f054ef923cd203cdc03a19eeab9e967f85f3a
chform <70721279+chform@users.noreply.github.com>
2021-03-29 16:02:37 +0200

    Add comments and JPEG extension
    Add comments.
    Add JPEG file extension in switch case.

--- ConvertToWebp.php
+++ ConvertToWebp.php
@@ -1,12 +1,15 @@
 <?php
+	// display error msg 
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
+	// function for convert files (ex: landscape.png -> landscape.png.webp)
 	function convertToWebp($path){
-		if(!file_exists($path.".webp")){
+		if(!file_exists($path.".webp")){ // we check if file already exist (for don't create erase and recreate)
 			$i = pathinfo($path);
 			switch($i["extension"]) {
 		    	case "jpg":
+			case "jpeg":
 		        	$im = imagecreatefromjpeg($path);
 		        	break;
 		    	case "png":
@@ -24,14 +27,13 @@
 				imagedestroy($im);
 				echo "<p>$path converted.<p>";
 			}
-		}else{
+		}else{ // if image already exist
 			echo "<p>$path.webp already exist.<p>";
 		}
 	}
-	$files = scandir("./");
-	foreach($files as $key => $value) {
-		if(is_file($value)==true){
-			//echo  "<p>$value</p>";
+	$files = scandir("./"); // folder to scan
+	foreach($files as $key => $value){ // check element per element
+		if(is_file($value)==true){ // we check if it's a file and not a folder
 			convertToWebp($value);
 		}
 	}
